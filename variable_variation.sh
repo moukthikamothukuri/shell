8ccc1353482f3d21f50561298642a611a47d2970
Moukthika <moukthikamothukuri@gmail.com>
2025-06-22 01:13:24 +0530

    It is easy

---
+++ variable_variation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=$1 #there should be no space before and after the =
+PERSON2=$2 
+
+echo "$PERSON1:: I am angry"
+echo "$PERSON2:: why what is the matter"
+echo "$PERSON1: : It is just I can't control my emotion'"
+echo "$PERSON2::don't worry, just take a deep breath, everything will be alright'"
