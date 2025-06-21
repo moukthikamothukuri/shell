a9747a1d9dcc4a2d9176574087c3c3c7581d618e
Moukthika <moukthikamothukuri@gmail.com>
2025-06-22 00:52:13 +0530

    It is easy

--- conversation.sh
+++ conversation.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+echo "apple: I am angry"
+echo "mona: why what is the matter"
+echo "apple : It is just I can't control my emotion'"
+echo "mona:don't worry, just take a deep breath, everything will be alright'"
