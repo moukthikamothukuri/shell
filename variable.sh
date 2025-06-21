168e56944a4d14716bacc9cf769a9f1ac6f257f9
Moukthika <moukthikamothukuri@gmail.com>
2025-06-22 01:02:24 +0530

    It is easy

--- variable.sh
+++ variable.sh
@@ -4,7 +4,7 @@
 Person1 = appy
 Person2 = mona 
 
-echo "$Person1: I am angry"
-echo "$Person2: why what is the matter"
-echo "$Person1 : It is just I can't control my emotion'"
-echo "$Person2:don't worry, just take a deep breath, everything will be alright'"
+echo "$Person1:: I am angry"
+echo "$Person2:: why what is the matter"
+echo "$Person1: : It is just I can't control my emotion'"
+echo "$Person2::don't worry, just take a deep breath, everything will be alright'"
