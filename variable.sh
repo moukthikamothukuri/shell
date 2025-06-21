3ee9df5cbe0c8ca11835e6d1fff991ecf560d0e7
Moukthika <moukthikamothukuri@gmail.com>
2025-06-22 00:59:57 +0530

    It is easy

---
+++ variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+Person1 = appy
+Person2 = mona 
+
+echo "$Person1: I am angry"
+echo "$Person2: why what is the matter"
+echo "$Person1 : It is just I can't control my emotion'"
+echo "$Person2:don't worry, just take a deep breath, everything will be alright'"
