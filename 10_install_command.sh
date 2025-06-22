60c6adc445baea2cddb136486da57f1cf54b6306
Moukthika <moukthikamothukuri@gmail.com>
2025-06-22 17:46:32 +0530

    It is easy

--- 10_install_command.sh
+++ 10_install_command.sh
@@ -10,6 +10,11 @@ else
 echo "you are good to go"
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0]
+then
+echo "it is installing , please wait"
 dnf install mysql -y
 
 if [ $? -eq 0 ]
@@ -20,3 +25,5 @@ echo "fail"
 exit 1
 
 fi
+else
+echo " Is is already present , you have nothing to do with it"
