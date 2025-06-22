be3f682922ffed4a5d62b15a3f4067e915060335
Moukthika <moukthikamothukuri@gmail.com>
2025-06-22 16:30:36 +0530

    It is easy

---
+++ 09_condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+#lt -less than
+#gt - greater than
+#-eq - equal
+#-ne - not equal
+if [ $NUMBER -lt 100 ]
+then
+echo "$NUMBER is a small number"
+else 
+echo "$NUMBER is a big number"
